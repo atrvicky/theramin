9be161bcabb05511434fa966f5da337da09d3e93
MK <atrvicky@live.com>
2020-07-29 02:55:44 +0530

    Final

--- theramin.ino
+++ theramin.ino
@@ -8,7 +8,7 @@ const int PING_PIN = 7; // Trigger Pin of Ultrasonic Sensor
 const int ECHO_PIN = 6; // Echo Pin of Ultrasonic Sensor
 
 // the minimum distance to be sensed by the Ultrasonic Sensor
-const int US_MIN = 0;
+const int US_MIN = 200;
 // the maximum distance to be sensed by the Ultrasonic Sensor
 const int US_MAX = 2700;
 
@@ -22,8 +22,14 @@ const int CC_OFF = 0;
 const int CC_ON = 65;
 const int CC_SUPER = 127;
 
-// a variable to maintain threshold
-const int THRESHOLD = 5;
+// a variable to maintain threshold (min 10)
+const int THRESHOLD = 25;
+
+// the button that controls the input (pulled down with a 1K resistor)
+const int BUTTON_PIN = 10;
+
+// the test led at 13
+const int TEST_LED = 11;
 
 int CC_MAPPED_VALUE = 0;
 int CC_CONTROL_VALUE = 0;
@@ -35,17 +41,46 @@ void setup()
     // Serial.begin(9600);
     MIDI.begin(MIDI_CHANNEL_OMNI); // Listen to all incoming messages
     MIDI.sendControlChange(CC_CONTROL_NUMBER, CC_OFF, CC_CHANNEL);
+    pinMode(BUTTON_PIN, INPUT);
+    pinMode(TEST_LED, OUTPUT);
 }
 
 void loop()
 {
+    int buttonState = digitalRead(BUTTON_PIN);
+    digitalWrite(TEST_LED, buttonState);
+
     long secs = pingSensor();
     CC_CONTROL_VALUE = ceil(map(secs, US_MIN, US_MAX, CC_OFF, CC_SUPER));
     limitCC();
+
+    if (buttonState == HIGH)
+    {
+        OLD_CC_VAL = 0;
+        CC_CONTROL_VALUE = 1;
+    }
     if (CC_CONTROL_VALUE != OLD_CC_VAL)
     {
-        MIDI.sendControlChange(CC_CONTROL_NUMBER, CC_CONTROL_VALUE, CC_CHANNEL);
-        OLD_CC_VAL = CC_CONTROL_VALUE;
+        if (abs(CC_CONTROL_VALUE - OLD_CC_VAL) < THRESHOLD)
+        {
+            if (OLD_CC_VAL < CC_CONTROL_VALUE)
+            {
+                for (int i = OLD_CC_VAL; i <= CC_CONTROL_VALUE; i++)
+                {
+                    MIDI.sendControlChange(CC_CONTROL_NUMBER, i, CC_CHANNEL);
+                    delay(5);
+                }
+            }
+            else
+            {
+                for (int i = OLD_CC_VAL; i >= CC_CONTROL_VALUE; i--)
+                {
+                    MIDI.sendControlChange(CC_CONTROL_NUMBER, i, CC_CHANNEL);
+                    delay(5);
+                }
+            }
+            OLD_CC_VAL = CC_CONTROL_VALUE;
+        }
     }
     delay(LOOP_DELAY);
 }
